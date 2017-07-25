cec68d12bdf30395f2431f404df34873e86f0b69
Tyler Nicewarner <tylernicewarner@yahoo.com>
2017-07-25 14:55:03 -0500

    Worked On Framework
    Continued to work on bot framework and started the points system

---
+++ TerraBot/Points.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Discord.WebSocket;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerraBot
+{
+
+    public class Points
+    {
+        public struct Member
+        {
+            public string name;
+            public ulong id;
+            public ulong points;
+        }
+
+        public List<Member> members = new List<Member>();
+
+        public void LoadPoints()
+        {
+            //read config.xml and load data into list
+        }
+
+        public void SavePoints()
+        {
+            //Rewrite the config.xml file
+        }
+
+        public void AddMember(string name, ulong id, ulong points = 0)
+        {
+            Member newMember;
+
+            newMember.name = name;
+            newMember.id = id;
+            newMember.points = points;
+
+            members.Add(newMember);
+        }
+
+        public void AddPoints(ref Member member, ulong points)
+        {
+            member.points += points;
+        }
+
+        public Member FindMember(ulong id)
+        {
+            //Simple Search Alg For User In List
+            return new Member();
+        }
+        
+    }
+}
--- TerraBot/Program.cs
+++ TerraBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Discord;
+using Discord.WebSocket;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,57 @@ namespace TerraBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        public async Task MainAsync()
         {
+            //Initalize and Setup Client
+            var config = new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info,
+                MessageCacheSize = 100
+            };
+            client = new DiscordSocketClient(config);
+
+            client.Log += Log;
+            client.MessageReceived += MessageRecieved;
+            client.MessageUpdated += MessageUpdated;
+
+            string token = "";
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            client.Ready += () =>
+            {
+                Console.WriteLine("Bot Connected!");
+                return Task.CompletedTask;
+            };
+
+            await Task.Delay(-1);
         }
+
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{message} -> {after}");
+        }
+
+        private async Task MessageRecieved(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong");
+            }
+           
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+       
     }
 }
