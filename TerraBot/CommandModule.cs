1674e5a52ef1c771ed20e7c40e92cf1997c7e997
Tyler Nicewarner <tylernicewarner@yahoo.com>
2017-07-26 14:04:59 -0500

    idk

--- TerraBot/CommandModule.cs
+++ TerraBot/CommandModule.cs
@@ -4,15 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Discord.Audio;
+using Discord.WebSocket;
+using Discord;
 
 namespace TerraBot
 {
     class CommandModule : ModuleBase
     {
+
         [Command("ping"), Summary("Pings The Bot")]
         public async Task PingCommand()
         {
             await ReplyAsync("pong!");
+        }
+
+        [Command("join"), Summary("Joins A Voice Channel")]
+        public async Task JoinChannel(IVoiceChannel channel = null)
+        {
+            channel = channel ?? (msg.Author as IGuildUser)?.VoiceChannel;
+
         }
     }
 }
---
+++ TerraBot/Commands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace TerraBot
+{
+    //Not sure if nessisary
+    class Commands
+    {
+        public async Task Install(DiscordSocketClient client)
+        {
+            System.IServiceProvider serviceCollection;
+
+            serviceCollection.AddSingleton(client);
+        }
+    }
+}
